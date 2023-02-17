a0407270d28b6a37ab316cde8f31c2abd4b4b547
Nwaeze Joshua <nwaezejoshua@gmail.com>
2023-02-17 17:26:23 +0100

    Delete src directory

--- src/routes/user-googleAuth.js
+++
@@ -1,7 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-// const googleAuth = ("../controllers/user-googleAuth.js");
-
-// router.post("/login", googleAuth.signInWithRedirect);
-
-// module.exports = router;
